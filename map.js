3e0163a5528b56cc2c4c158b0ef2099fcaf3964c
Christian Heyn <christian@billomat.com>
2019-02-05 16:23:25 +0100

    init repo

---
+++ map.js
@@ -0,0 +1,25 @@
+const invoiceData = [
+    { id: 0, status: 'DRAFT', invoice_number: 'RE01'},
+    { id: 1, status: 'OPEN', invoice_number: 'RE02'},
+    { id: 2, status: 'OVERDUE', invoice_number: 'RE03'},
+    { id: 3, status: 'PAID', invoice_number: 'RE04'},
+    { id: 5, status: 'CANCELED', invoice_number: 'RE06'},
+];
+
+/* Duplicated for every status */
+// const setDraftStatus = (invoiceObj) => {
+//     return {...invoiceObj, status: 'DRAFT'};
+// };
+
+const isOpen = x => x.status === 'OPEN';
+const makeSetStatusIf = status => predicat => invoiceObj => {
+    return (predicat(invoiceObj))
+                ? { ...invoiceObj, status }
+                : invoiceObj
+};
+
+const setDraftStatusIf = makeSetStatusIf('DRAFT');
+
+const result = invoiceData.map(setDraftStatusIf(isOpen));
+
+console.log(result);
